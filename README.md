1d1ead5a12db43a29b8a361fe02ac43dbaa5cb73
ujnss <unlinunlin@163.com>
2024-12-26 09:17:53 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # zktls-core-sdk
+
+## installation
+
+```sh
+npm install
+```
+
+
+## Usage
+
+See [test_local_1.js](./test_local_1.js).
---
+++ primus_zk.js
@@ -0,0 +1,82 @@
+// support websocket
+global.WebSocket = require('ws');
+
+// load wasm module
+Module_callAlgorithm = null;
+const Module = require("./client_plugin.js");
+Module.onRuntimeInitialized = async () => {
+  console.log("Module Initialized OK.");
+  Module_callAlgorithm = Module.cwrap('callAlgorithm', 'string', ['string']);
+}
+
+const callAlgorithm = async (params) => {
+  if (!Module_callAlgorithm) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
+
+  return Module_callAlgorithm(params);
+};
+
+
+const init = async () => {
+  const params = `{"method":"init","version":"1.1.1","params":{}}`;
+  console.log('enter init. params:', params);
+  const result = await callAlgorithm(params);
+  console.log('leave init. result:', result);
+  return result;
+};
+
+
+exports.getAttestation = async (paramsObj) => {
+  await init();
+
+  const _paramsObj = { method: "getAttestation", version: "1.1.1", params: paramsObj };
+  const params = JSON.stringify(_paramsObj);
+  console.log('enter getAttestation. params:', params);
+  const result = await callAlgorithm(params);
+  console.log('leave getAttestation. result:', result);
+  return result;
+};
+
+// todo: Promise
+exports.getAttestationResult = async () => {
+  const params = `{"method":"getAttestationResult","version":"1.1.1","params":{"requestid":"1"}}`;
+  console.log('enter getAttestationResult. params:', params);
+  const result = await callAlgorithm(params);
+  console.log('leave getAttestationResult. result:', result);
+  return result;
+};
+
+exports.getAttestationConfig = () => {
+  const attestationParams = {
+    source: "source", // not empty
+    requestid: "d9415490-3bc3-49ac-93ca-97165aa2a4a1", // todo:auto generate
+    padoUrl: "wss://api-dev.padolabs.org/algorithm-proxyV2", // should set
+    proxyUrl: "wss://api-dev.padolabs.org/algoproxyV2", // should set
+    basePort: "443",
+    getdatatime: "1735028372985", // todo:auto generate
+    credVersion: "1.0.5",
+    user: {
+      userid: "1111111111111111111",
+      address: "0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+      token: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    },
+    authUseridHash: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    appParameters: { // should set
+      appId: "0x3333333333333333333333333333333333333333",
+      appSignParameters: "{}",
+      appSignature: "0xcccccccccccccccccccccccccccccccccccccccc",
+      additionParams: ""
+    },
+    reqType: "web",
+    host: "localhost.com", // should set
+    requests: [], // should set
+    responses: [], // should set
+    templateId: "5555555555555555555",
+    PADOSERVERURL: "https://api-dev.padolabs.org",
+    padoExtensionVersion: "0.3.19"
+  }
+  return attestationParams;
+}
+
+
