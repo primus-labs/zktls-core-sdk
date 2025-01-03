5369217e6781827df982fe4e811cd6705569957a
fksyuan <fksyuan@163.com>
2025-01-03 20:55:04 +0800

    update sdk

---
+++ src/startatttest.ts
@@ -0,0 +1,36 @@
+import { PrimusCoreTLS} from './index';
+
+const appId = "0x899dd126268e3010beaa1ac141a2a0aa98deba09";
+const appSecret = "0x7da5d1cd2fdd494aa1176031151a6202734e30ddb14fd01dc3376616408ee0a7";
+
+async function test() {
+    const zkTLS = new PrimusCoreTLS();
+        // const result =
+            await zkTLS.init(appId, appSecret);
+        // console.log("-------------init result=", result);
+        // 2.
+
+        let request = {
+            url: 'https://www.binance.com/bapi/accounts/v2/public/account/ip/country-city-short',
+            method: 'GET',
+            body: '',
+            header: {}
+        }
+        const responseResolves = [{
+            keyName: 'code',
+            parsePath: '$.code',
+            parseType: 'string'
+        }]
+        const userAddr = '0x3B86401865D9C17A51C51D6D5f5aabA733Dd8E14'
+        const generateRequestParamsRes = zkTLS.generateRequestParams(request, responseResolves, userAddr)
+
+        // console.log("-------------generateRequestParams result=", generateRequestParamsRes);
+        // 3.
+        // const startAttestationRes =
+        // const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+        // await delay(800);
+
+        await zkTLS.startAttestation(generateRequestParamsRes)
+}
+
+test();
