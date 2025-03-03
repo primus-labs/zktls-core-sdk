b3949d2a5c50d5b3d2b0d4526e9f20ee427edbfb
fksyuan <fksyuan@163.com>
2025-03-03 20:03:25 +0800

    add the cipher default value

--- src/classes/AttRequest.ts
+++ src/classes/AttRequest.ts
@@ -24,6 +24,7 @@ export class AttRequest {
     }
     this.request = request
     this.responseResolves = responseResolves
+    this.sslCipher = "ECDHE-RSA-AES128-GCM-SHA256";
   }
   setAdditionParams(additionParams: string) {
     this.additionParams = additionParams
