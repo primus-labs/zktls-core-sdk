8fd0c0b7756ec96f8f88c29c183609fc6f1ee4e3
ujnss <unlinunlin@163.com>
2025-06-17 06:13:36 +0000

    chore: remove useless

--- addon.cc
+++ addon.cc
@@ -10,12 +10,7 @@ std::string __callAlgorithm(const std::string &input) {
   return std::string(callAlgorithm(input.c_str()));
 }
 
-// Function pointer type
-typedef std::string (*TestFunction)(const std::string &);
-
-// Node-API wrapper for the `test` function
 Napi::String CallAlgorithmWrapped(const Napi::CallbackInfo &info) {
-  printf("CallAlgorithmWrapped\n");
   Napi::Env env = info.Env();
 
   // Ensure one argument is provided
@@ -24,22 +19,15 @@ Napi::String CallAlgorithmWrapped(const Napi::CallbackInfo &info) {
     return Napi::String::New(env, "");
   }
 
-  // Convert the JavaScript string to a C++ string
   std::string input = info[0].As<Napi::String>().Utf8Value();
-
-  // Call the dynamic library function
   std::string result = __callAlgorithm(input);
 
-  // Return the result as a JavaScript string
   return Napi::String::New(env, result);
 }
 
-// Initialize the module
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  printf("Init\n");
-
-    exports.Set(Napi::String::New(env, "callAlgorithm"),
-                Napi::Function::New(env, CallAlgorithmWrapped));
+  exports.Set(Napi::String::New(env, "callAlgorithm"),
+              Napi::Function::New(env, CallAlgorithmWrapped));
   return exports;
 }
 
