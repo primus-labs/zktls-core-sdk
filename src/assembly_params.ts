cb469dad50bfe44504564c3ec05850324bf66efe
ysm <ysm@ysm.com>
2026-01-28 11:09:52 +0800

    fix: setAttConditions

--- src/assembly_params.ts
+++ src/assembly_params.ts
@@ -1,11 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
-import { AttNetworkRequest, AttNetworkResponseResolve, SignedAttRequest } from './index.d'
+import { AttNetworkRequest, AttNetworkResponseResolve, SignedAttRequest } from './index.d';
 import { AlgorithmUrls } from './classes/AlgorithmUrls';
+
+interface AttConditionItem {
+    field: string;
+    op: string;
+    value: string;
+}
 export function assemblyParams(att: SignedAttRequest, algorithmUrls: AlgorithmUrls) {
     let { primusMpcUrl, primusProxyUrl, proxyUrl } = algorithmUrls
     let padoUrl = primusProxyUrl;
     let modelType = "proxytls";
-    const { attRequest: { request, responseResolves, attMode, userAddress, appId, additionParams, sslCipher, noProxy, requestInterval }, appSignature } = att
+    const { attRequest: { request, responseResolves, attMode, userAddress, appId, additionParams, sslCipher, noProxy, requestInterval, attConditions }, appSignature } = att
     const requestUrl = Array.isArray(request) ? request[0].url : request.url;
     let host = new URL(requestUrl).host;
     const requestid = uuidv4();
@@ -41,7 +47,7 @@ export function assemblyParams(att: SignedAttRequest, algorithmUrls: AlgorithmUr
         reqType: "web",
         host,
         requests: assemblyRequest(request),
-        responses: assemblyResponse(responseResolves),
+        responses: assemblyResponse(responseResolves, (attConditions as AttConditionItem[][])),
         templateId: "",
         padoExtensionVersion: "0.3.21",
         cipher: sslCipher,
@@ -52,7 +58,7 @@ export function assemblyParams(att: SignedAttRequest, algorithmUrls: AlgorithmUr
 
 function assemblyRequest(request: AttNetworkRequest | AttNetworkRequest[]) {
     const requests = Array.isArray(request) ? request : [request]
-    return requests.map(({ url, header, method, body }) => ({
+    return requests.map(({ url, header, method, body }, idx) => ({
         url,
         method,
         headers: {
@@ -60,6 +66,7 @@ function assemblyRequest(request: AttNetworkRequest | AttNetworkRequest[]) {
             'Accept-Encoding': 'identity',
         },
         body,
+        name: `${url}-${idx}`
     }))
 }
 
@@ -73,7 +80,7 @@ function _getOp(op?: string) {
     if (op === "SHA256_EX") {
         return "REVEAL_HEX_STRING";
     }
-    return op ?? 'REVEAL_STRING';
+    return op ? op: 'REVEAL_STRING';
 }
 function _getType(op?: string) {
     if (['>', '>=', '=', '!=', '<', '<=', 'STREQ', 'STRNEQ'].includes(op ?? "")) {
@@ -84,29 +91,41 @@ function _getType(op?: string) {
     return "FIELD_REVEAL"
 }
 
-function assemblyResponse(responseResolves: AttNetworkResponseResolve[] | AttNetworkResponseResolve[][]) {
+function assemblyResponse(responseResolves: AttNetworkResponseResolve[] | AttNetworkResponseResolve[][], attConditions: AttConditionItem[][] = []) {
     const groups = Array.isArray(responseResolves[0])
         ? responseResolves as AttNetworkResponseResolve[][]
-        : [responseResolves as AttNetworkResponseResolve[]]
-    return groups.map(group => {
+        : [responseResolves as AttNetworkResponseResolve[]];
+    return groups.map((group, idx) => {
+        const urlItemConditions: AttConditionItem[] | undefined = attConditions[idx];
         const subconditions = group.map(rR => {
-            const { keyName, parsePath, op } = rR
-
-            return {
-                field: _getField(parsePath, op),
-                reveal_id: keyName,
-                op: _getOp(op),
-                type: _getType(op),
+            let itemOp = '';
+            let itemValue = '';
+            const { keyName, parsePath } = rR;
+            if (urlItemConditions && Array.isArray(urlItemConditions)) {
+                // Find matching condition by field name
+                const matchingCondition = urlItemConditions.find((cond) => cond.field === keyName);
+                if (matchingCondition) {
+                    itemOp = matchingCondition.op;
+                    itemValue = matchingCondition.value;
+                }
             }
-        })
+            return {
+                field: _getField(parsePath, itemOp),
+                reveal_id: keyName,
+                op: _getOp(itemOp),
+                type: _getType(itemOp),
+                value: itemValue,
+            };
+        });
 
         return {
             conditions: {
                 type: "CONDITION_EXPANSION",
-                op: "&",
+                op: "BOOLEAN_AND",
+                // op:  Array.isArray(responseResolves[0])?"BOOLEAN_AND": "&",
                 subconditions,
             },
-        }
-    })
+        };
+    });
 }
 
